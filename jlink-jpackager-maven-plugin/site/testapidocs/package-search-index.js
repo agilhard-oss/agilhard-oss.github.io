639b177835dbe58ab042056ba74275b3a59b8f58
Bernd Eilers <bernd@bernd-eilers.net>
2019-02-02 17:58:39 +0100

    update documentation

--- jlink-jpackager-maven-plugin/site/testapidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.agilhard.maven.plugins.jpacktool.mojo.base"}]
--- jlink-jpackager-maven-plugin/site/testapidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"net.agilhard.maven.plugins.jpacktool.mojo.base","l":"AbstractPackageToolMojoTest"},{"l":"All Classes","url":"allclasses-index.html"}]
